0a63f1d5016a52393aaf1e0cd461650f87ff6581
LoganFehringer <61851689+LoganFehringer@users.noreply.github.com>
2020-03-05 17:02:11 -0600

    Update ALU.sv

--- srcs/ALU.sv
+++ srcs/ALU.sv
@@ -1,11 +1,14 @@
-module ALU(A,B,F,Y);
+module ALU(A,B,F,Y Ovf);
 input logic [31:0] A,B;
 input logic [2:0] F;
 output logic [31:0] Y;
+output logic Ovf;
 logic [31:0] sum,Bout;
 assign Bout = F[2]? ~B:B; // Determines if B is + or -
 assign sum = A + Bout + F[2]; // Adds A and Bout for 1's comp and if F[2] = 1 then it makes 2's comp
 assign z = (Y==32'b0); // checks if all bits are 0
+assign Temp = {1'b0,A}+{1'b0,Bout};
+assign Ovf = Temp[32];
 always @ (*)
 case(F[1:0])
 2'b00 : Y = A & Bout; // Ands A and B or ~B
