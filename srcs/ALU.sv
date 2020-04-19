45e32f0e916c43980f4d70bb70ae2a4e6b678ba0
jarrett <jj_atkinson@att.net>
2020-04-18 23:02:58 -0500

    Fix some compiler errors

--- srcs/ALU.sv
+++ srcs/ALU.sv
@@ -1,16 +1,19 @@
-module ALU #(parameter N = 32)(A,B,F,Y, Ovf);
+module ALU #(parameter N = 32)(A,B,F,Y, Ovf, Zero);
 
 	input logic [N-1:0] A,B;
 	input logic [2:0] F;
 	output logic [N-1:0] Y;
-	output logic Ovf;
+	output logic Ovf, Zero;
 	logic [N-1:0] sum,Bout;
+	wire [N-1:0] Temp;
+	
 
 assign Bout = F[2]? ~B:B;     // Determines if B is + or -
 assign sum = A + Bout + F[2]; // Adds A and Bout for 1's comp and if F[2] = 1 then it makes 2's comp
 	assign z = (Y=={N{1'b0}});        // checks if all bits are 0
 assign Temp = {1'b0,A}+{1'b0,Bout};
 	assign Ovf = Temp[N];
+	assign Zero = Y == 0;
 
 always @ (*)
 	case(F[1:0])
